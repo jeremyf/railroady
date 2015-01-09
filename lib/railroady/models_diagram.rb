d93e42df5f5a70f1dd1672e528e21260ca90aac9
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-01-09 10:59:53 -0500

    Adding behavior for namespaced models in app

--- lib/railroady/models_diagram.rb
+++ lib/railroady/models_diagram.rb
@@ -43,7 +43,7 @@ class ModelsDiagram < AppDiagram
 
 
   def extract_class_name(filename)
-    filename.split('/').last.camelize.chomp(".rb")
+    filename.sub(%r{^.*app/models/}, '').chomp(".rb").camelize
   end
 
   
